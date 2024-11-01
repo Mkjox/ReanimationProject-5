828e027499fe0ed7d763bd7fd4dad64be2807150
jox <zincir_890@hotmail.com>
2024-11-01 17:48:10 +0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useRef } from 'react';
+import { Dimensions, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView, TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withDelay, withSpring, withTiming } from 'react-native-reanimated';
+
+const { width: SIZE } = Dimensions.get('window')
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 export default function App() {
+  const scale = useSharedValue(0);
+  const opacity = useSharedValue(1);
+
+  const doubleTapRef = useRef();
+
+  const rStyle = useAnimatedStyle(() => ({
+    transform: [
+      { scale: Math.max(scale.value, 0) }
+    ]
+  }));
+
+  const rTextStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value
+  }));
+
+  const onSingleTap = useCallback(() => {
+    opacity.value = withTiming(0, undefined, (isFinished) => {
+      if (isFinished) {
+        opacity.value = withDelay(500, withTiming(0));
+      }
+    });
+  }, []);
+
+  const onDoubleTap = useCallback(() => {
+    scale.value = withSpring(1, undefined, (isFinished) => {
+      if (isFinished) {
+        scale.value = withDelay(500, withSpring(1));
+      }
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GestureHandlerRootView>
+        <TapGestureHandler
+          waitFor={doubleTapRef}
+          onActivated={onSingleTap}
+        >
+          <TapGestureHandler
+            maxDelayMs={250}
+            ref={doubleTapRef}
+            numberOfTaps={2}
+            onActivated={onDoubleTap}>
+            <Animated.View style={styles.container}>
+              <ImageBackground source={require('./assets/image.jpeg')} style={styles.image}>
+                <AnimatedImage
+                  source={require('./assets/heart.png')}
+                  style={[
+                    styles.image,
+                    {
+                      shadowOffset: { width: 0, height: 20 },
+                      shadowOpacity: 0.35,
+                      shadowRadius: 35
+                    },
+                    rStyle
+                  ]}
+                  resizeMode='center' />
+              </ImageBackground>
+              <Animated.Text style={[styles.moyai, rTextStyle]}>🗿🗿🗿🗿</Animated.Text>
+            </Animated.View>
+          </TapGestureHandler>
+        </TapGestureHandler>
+      </GestureHandlerRootView>
     </View>
   );
 }
@@ -17,4 +83,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: SIZE,
+    height: SIZE
+  },
+  moyai: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 35
+  }
 });
